80c472a107e8e456e6a0eaee891df4c1d03c7d3b
Jorge E Villaverde <jorgevillaverde@gmail.com>
2018-02-18 12:32:45 -0300

    Savings Account

---
+++ java/loan-app/savings-account-service/src/main/java/ar/edu/utn/frre/dacs/loan/savings/model/SavingsAccount.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2015-2018 UTN-FRRe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.edu.utn.frre.dacs.loan.savings.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class SavingsAccount implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// Properties -------------------------------------------------------------
+	
+	@Id
+	private Long number;
+	
+	private Long clientId;
+	
+	private String name;
+	
+	private BigDecimal balance;
+	
+	@OneToMany(mappedBy="account", fetch = FetchType.LAZY)
+	private Set<Transaction> transactions;
+
+	// Getters/Setters --------------------------------------------------------
+
+	public Long getNumber() {
+		return number;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public Long getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Long clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public Set<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(Set<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
+}
