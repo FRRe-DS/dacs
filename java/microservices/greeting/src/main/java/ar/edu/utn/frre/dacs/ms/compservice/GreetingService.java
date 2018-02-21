4500c88c3f8c3035573b17e39c37030ffc2932ac
d97villaje <d97villaje@S97060SI.nbchad.local>
2018-02-21 09:13:47 -0300

    Agregar HystrixCommand

--- java/microservices/greeting/src/main/java/ar/edu/utn/frre/dacs/ms/compservice/GreetingService.java
+++ java/microservices/greeting/src/main/java/ar/edu/utn/frre/dacs/ms/compservice/GreetingService.java
@@ -15,22 +15,6 @@
  */
 package ar.edu.utn.frre.dacs.ms.compservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Api {
-	
-	protected Logger logger = LoggerFactory.getLogger(Api.class.getName());
-	
-	@Autowired
-	private GreetingService service;
-	
-	@GetMapping("/greeting")
-	public String sayHi() {
-		return service.sayHi();
-	}
+public interface GreetingService {
+	String sayHi();
 }
